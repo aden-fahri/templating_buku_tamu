d296275348bd88eff9416baa7f6f40c1e4bccebf
= <=>
2025-09-11 09:05:56 +0700

    menambahkan modal dan fitur tambah data tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -9,10 +9,29 @@
    <!-- Page Heading -->
    <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
 
+   <?php
+    // Jika ada tombol simpan
+    if (isset($_POST['simpan'])) {
+      if (tambah_tamu($_POST) > 0) {
+    ?>
+       <div class="alert alert-success" role="alert">
+         Data berhasil disimpan!
+       </div>
+   <?php
+      }
+    }
+    ?>
+
    <!-- DataTales Example -->
    <div class="card shadow mb-4">
      <div class="card-header py-3">
-       <h6 class="m-0 font-weight-bold text-primary">Daftar Tamu</h6>
+       <button type="button" class="btn btn-primary btn-icon-split"
+         data-toggle="modal" data-target="#tambahModal">
+         <span class="icon text-white-50">
+           <i class="fas fa-plus"></i>
+         </span>
+         <span class="text">Data Tamu</span>
+       </button>
      </div>
      <div class="card-body">
        <div class="table-responsive">
@@ -56,6 +75,76 @@
 
  </div>
  <!-- /.container-fluid -->
+
+ <?php
+  // mangambil data barang dari tabel dengan kode tarbesar
+  $query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar From buku_tamu");
+  $data = mysqli_fetch_array($query);
+  $kodeTamu = $data['kodeTerbesar'];
+
+  // mengambil angka dari kode barang terbesar
+  $urutan = (int) substr($kodeTamu, 2, 3);
+
+  //nomor yang diambil akan bertambah 1 dan menentukan urutannya
+  $urutan++;
+
+  //membuat kode barang baru
+  $huruf = "xt";
+  $kodeTamu = $huruf . sprintf("%03s", $urutan);
+  ?>
+
+ <!-- Modal Tambah-->
+ <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         <form method="post" action="">
+           <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+           <div class="form-group row">
+             <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="nama_tamu" id="nama_tamu">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+             <div class="col-sm-8">
+               <textarea class="form-control" name="alamat" id="alamat"></textarea>
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="no_hp" id="no_hp">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="bertemu" id="bertemu">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="kepentingan" id="kepentingan">
+             </div>
+           </div>
+           <div class="modal-footer">
+             <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+             <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+           </div>
+         </form>
+       </div>
+     </div>
+   </div>
+ </div>
 
 
  <?php
