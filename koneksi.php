2b9fd032f81229d01932737117cb840961b501f0
= <=>
2025-09-14 12:40:33 +0700

    memperbaiki koneksi yang di panggil terus menerus sehingga menyebabkan Warning!

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,8 @@
 <?php
-define('HOST_NAME', 'localhost');
-define('USER_NAME', 'root');
-define('PASSWORD', '');
-define('DB_NAME', 'app_bukutamu');
+require_once('conn.php');
 
 $koneksi = mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME);
+
+if (!$koneksi) {
+  die('koneksi gagal: ' . mysqli_connect_error());
+}
